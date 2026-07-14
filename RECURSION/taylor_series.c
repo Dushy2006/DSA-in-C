f2cb84e20735787391064ed157659dd73590ccdf
Dushy2006 <dushyanthraj2006@gmail.com>
2026-07-14 14:26:42 +0530

    taylor series.c

---
+++ RECURSION/taylor_series.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+float e(int x,int n)
+{
+    static  float p=1,f=1;
+    float r;
+    if(n==0)
+    {
+        return 1;
+    }
+    else{
+        r=e(x,n-1);
+        p=p*x;
+        f=f*n;
+        return r+(p/f);
+    }
+
+}
+int main(){
+    printf("%lf",e(4,15));
+
+}
