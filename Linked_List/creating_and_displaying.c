2b8d13a0e5ea85662216fe41c820a2ebac944f8e
Dushy2006 <dushyanthraj2006@gmail.com>
2026-08-06 19:29:00 +0530

    display_recursively.c

---
+++ Linked_List/creating_and_displaying.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+};
+void head1(struct node *head,int val){
+    
+    
+    head->data=val;
+    head->next=NULL;
+    
+
+
+
+}
+void insert_at_end(struct node*head,int val){
+    struct node *t,*last;
+    t=(struct node*)malloc(sizeof(struct node));
+    if(t==NULL){
+        printf("Memory allocation failed");
+        return;
+    }
+    
+        t->data=val;
+        t->next=NULL;
+        last=head;
+        while(last->next!=NULL)
+        {
+            last=last->next;
+
+        }
+        last->next=t;
+    
+
+}
+void display(struct node*head){
+    struct node *t=head;
+    while(t!=NULL)
+    {
+        printf("%d->",t->data);
+        t=t->next;
+    }
+    printf("NULL/n");
+
+}
+int main()
+{
+    struct node *head;
+    int a;
+    printf("Enter the number of nodes:");
+    scanf("%d",&a);
+    head=(struct node*)malloc(sizeof(struct node));
+
+
+    for(int i=1;i<=a;i++)
+    
+    {
+        if(i==1)
+        {
+
+            int val;
+
+        printf("Enter the value of node %d:",i);
+        scanf("%d",&val);
+        head1(head,val);
+        }
+        else{
+            int val;
+            printf("Enter the value of node %d:",i);
+            scanf("%d",&val);
+            insert_at_end(head,val);
+        }
+        
+    }
+    printf("Displaying linked list:");
+    display(head);
+
+return 0;
+}
